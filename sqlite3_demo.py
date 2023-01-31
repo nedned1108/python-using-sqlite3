17f9852492122be1932c8e9001cf941ef5175abf
nednguyen <rpmshockwave@gmail.com>
2023-01-30 19:10:27 -0500

    Use results performed from executing a SELECT statement on existing database

--- sqlite3_demo.py
+++ sqlite3_demo.py
@@ -4,8 +4,17 @@ import sqlite3
 DB_FILE = "dev.db"
 
 with sqlite3.connect(DB_FILE) as conn:
-  print(conn) # <sqlite3.Connection object at 0x7f43314a9990>
-  curs = conn.cursor()
-  curs.execute("SELECT 'Hello World!'")
-  result = curs.fetchone()
-  print(result) # ('Hello World!',)
+    print(conn)  # <sqlite3.Connection object at 0x7f43314a9990>
+
+    curs = conn.cursor()
+    # curs.execute("SELECT 'Hello World!'")
+    # result = curs.fetchone()
+    # print(result)  # ('Hello World!',)
+
+    curs.execute('SELECT manu_year, make, model FROM cars;')
+    cars = curs.fetchall()
+    for car in cars:
+        print(car)
+        # (1993, 'Mazda', 'Rx7')
+        # (1995, 'Mitsubishi', 'Eclipse')
+        # (1994, 'Acura', 'Integra')
