1f2671c18359d64be847fabc0ef0df81a95b6d41
nednguyen <rpmshockwave@gmail.com>
2023-01-30 19:05:23 -0500

    Open a cursor to perform data operations

--- sqlite3_demo.py
+++ sqlite3_demo.py
@@ -4,4 +4,8 @@ import sqlite3
 DB_FILE = "dev.db"
 
 with sqlite3.connect(DB_FILE) as conn:
-  print(conn) 
+  print(conn) # <sqlite3.Connection object at 0x7f43314a9990>
+  curs = conn.cursor()
+  curs.execute("SELECT 'Hello World!'")
+  result = curs.fetchone()
+  print(result) # ('Hello World!',)
